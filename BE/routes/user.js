d1d17ab54a9a9cc6ce635bdd6737c9e1ee395f99
Huwnga <hungbungbu2003@gmail.com>
2023-01-17 10:48:19 +0700

    api set role and get all role

--- BE/routes/user.js
+++ BE/routes/user.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const routes = express.Router();
 
+const isAdmin = require('../middlewares/isAdmin');
+const isUser = require('../middlewares/isUser');
+
 const userController = require('../controllers/user');
 
-routes.post('/createGroup', userController.postCreateGroup);
+routes.post('/createGroup', isUser, userController.postCreateGroup);
+routes.post('/setRole', isUser, userController.postSetRole);
+routes.get('/role', isUser, userController.getRole);
 
 module.exports = routes;
