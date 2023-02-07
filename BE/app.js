2a5e624e24b1e6e1db465fdcaa5ce63bea9525c2
Huwnga <hungbungbu2003@gmail.com>
2023-02-07 09:35:23 +0700

    add func room in socket

--- BE/app.js
+++ BE/app.js
@@ -49,47 +49,8 @@ io.on('connection', socket => {
   const token = socket.handshake.auth.token;
 
   if (socketFile.checkToken(token)) {
-    var data = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    var userId = data.userId;
-    var role = data.role;
-
-    if (role == 'user') {
-      User.findOne({
-        where: {
-          id: userId
-        },
-        include: Conversation
-      })
-        .then(user => {
-          if (user) {
-            if (user.status == 1) {
-              const conversations = user.conversations;
-
-              conversations.forEach(conversation => {
-                socket.join("conversation" + conversation.id);
-              });
-            } else {
-              socket.send(JSON.stringify({
-                action: 'joinRoom',
-                error: 'This Account is block!'
-              }));
-            }
-          } else {
-            socket.send(JSON.stringify({
-              action: 'joinRoom',
-              error: 'This Account Doesn\'t exists'
-            }));
-          }
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    } else {
-      socket.send(JSON.stringify({
-        action: 'joinRoom',
-        error: 'Don\'t have perrmission in this page'
-      }));
-    }
+    socketFile.joinRoomByToken(socket, token);
+    
     socket.connect();
     console.log('Client connected');
   } else {
--- BE/socket.js
+++ BE/socket.js
@@ -19,12 +19,60 @@ module.exports = {
   checkToken: (token) => {
     try {
       var data = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-      
+
       return true;
     } catch (err) {
       if (err) {
         return false;
       }
+    }
+  },
+
+  joinRoomByToken: (socket, token) => {
+    if (socket && token) {
+      var data = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+      var userId = data.userId;
+      var role = data.role;
+  
+      if (role == 'user') {
+        User.findOne({
+          where: {
+            id: userId
+          },
+          include: Conversation
+        })
+          .then(user => {
+            if (user) {
+              if (user.status == 1) {
+                const conversations = user.conversations;
+  
+                conversations.forEach(conversation => {
+                  socket.join("conversation" + conversation.id);
+                });
+              } else {
+                socket.send(JSON.stringify({
+                  action: 'joinRoom',
+                  error: 'This Account is block!'
+                }));
+              }
+            } else {
+              socket.send(JSON.stringify({
+                action: 'joinRoom',
+                error: 'This Account Doesn\'t exists'
+              }));
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      } else {
+        socket.send(JSON.stringify({
+          action: 'joinRoom',
+          error: 'Don\'t have perrmission in this page'
+        }));
+      }
+    } else {
+      throw new Error('Don\'t have socket or token!');
     }
   }
 }
